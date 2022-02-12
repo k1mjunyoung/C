c874215032d63723f369b1d3fefe26dfb74f8571
k1mjunyoung <74641191+k1mjunyoung@users.noreply.github.com>
2022-02-12 12:56:24 +0900

    20220212

--- 15Review2/02.c
+++ 15Review2/02.c
@@ -2,33 +2,42 @@
 
 #include <stdio.h>
 
-int main(void)
+void DemToBin(int num)
 {
-    int num;
-    int binaryArr[100];
-
-    printf("Enter a Number[10] : ");
-    scanf("%d", &num);
-
+    int binaryArr[100] = {0};
     int temp = num;
 
-    for (int i = 0; i < temp; i++)
+    for (int i = 0; (num / 2) > 0; i++)
     {
         binaryArr[i] = num % 2;
         num = num / 2;
-    }
-    
-    int length = sizeof(binaryArr[100]) / sizeof(int); // 출력을 위한 배열 길이
-    
-    printf("Binary length : %d\n", length);
 
-    printf("Your Number[2] : ");
-    for (int i = length; i >= 0 ; i--)
+        if (num < 2)
+            break;
+    }
+
+    int binaryLen = sizeof(binaryArr[100]) / sizeof(int);
+
+    printf("Binary length is %d\n", binaryLen);
+
+    printf("Your number[2] : ");
+
+    for (int i = 0; i < num; i++)
     {
         printf("%d", binaryArr[i]);
     }
-    
+
     printf("\n");
+}
+
+int main(void)
+{
+    int num;
+
+    printf("Enter a number[10] : ");
+    scanf("%d", &num);
+
+    DemToBin(num);
 
     return 0;
 }
