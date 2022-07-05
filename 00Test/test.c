b9f0620ccb77cbde7ac987924fa66087a1e7d556
k1mjunyoung <74641191+k1mjunyoung@users.noreply.github.com>
2022-07-05 17:39:34 +0900

    2022-07-05

--- 00Test/test.c
+++ 00Test/test.c
@@ -2,8 +2,19 @@
 
 int main(void)
 {
-    int arr[5];
+    int num = 10;
+    int *pnum;
+    pnum = &num;
+    int **dpnum;
+    dpnum = &pnum;
 
-    printf("%d\n", sizeof(arr));
+    printf("%p\n", &num);
+    printf("%p\n", pnum);
+    printf("%p\n\n", dpnum);
+
+    printf("%d\n", *pnum);
+    printf("%d\n", **dpnum);
+
+
     return 0;
 }
